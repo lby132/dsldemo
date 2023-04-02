af794c29d3b3b25ca45dfe1815ae097595459daf
lby132 <quddbscjswow@naver.com>
2023-04-03 00:30:14 +0900

    Initial commit

---
+++ src/main/java/com/example/querydsl/dsldemo/DsldemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.querydsl.dsldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsldemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsldemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/querydsl/dsldemo/DsldemoApplicationTests.java
@@ -0,0 +1,37 @@
+package com.example.querydsl.dsldemo;
+
+import com.example.querydsl.dsldemo.entity.Hello;
+import com.example.querydsl.dsldemo.entity.QHello;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+@SpringBootTest
+@Transactional
+class DsldemoApplicationTests {
+	
+	@Autowired
+	EntityManager em;
+
+	@Test
+	void contextLoads() {
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+		QHello qHello = new QHello("h");
+
+		Hello result = query
+				.selectFrom(qHello)
+				.fetchOne();
+
+		Assertions.assertThat(result).isEqualTo(hello);
+		Assertions.assertThat(result.getId()).isEqualTo(hello.getId());
+	}
+
+}
